ecbd38b3522a1b325317878281d002d4743b7e03
kids-app server <server@uniplay-kids.ru>
2026-07-05 15:19:00 +0000

    Add Dialogue Whats your name as 9th Spotlight 5 activity; drop decorative image + instruction blurb

--- components/LessonBuilder/ActivityRenderers/SpotlightHtmlActivityRenderer.tsx
+++ components/LessonBuilder/ActivityRenderers/SpotlightHtmlActivityRenderer.tsx
@@ -8,6 +8,7 @@ import SpotlightS5InternationalWords from './SpotlightS5InternationalWords';
 import SpotlightS5Alphabet1 from './SpotlightS5Alphabet1';
 import SpotlightS5SongReadingRules from './SpotlightS5SongReadingRules';
 import SpotlightS5FindWordsRightWrong from './SpotlightS5FindWordsRightWrong';
+import SpotlightS5DialogueName from './SpotlightS5DialogueName';
 import SpotlightEx1 from './SpotlightEx1';
 import SpotlightEx2 from './SpotlightEx2';
 import SpotlightEx3 from './SpotlightEx3';
@@ -80,6 +81,7 @@ const SPOTLIGHT_COMPONENTS: Record<string, React.ComponentType<SlideProps>> = {
   'spotlight5-alphabet-1': SpotlightS5Alphabet1,
   'spotlight5-song-reading-rules': SpotlightS5SongReadingRules,
   'spotlight5-find-words-right-wrong': SpotlightS5FindWordsRightWrong,
+  'spotlight5-dialogue-name': SpotlightS5DialogueName,
   'verb-be-ex1': SpotlightEx1,
   'verb-be-ex2': SpotlightEx2,
   'verb-be-ex3': SpotlightEx3,
