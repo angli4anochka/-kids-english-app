35b9bdab0fbd460122ab3acae723451a403c241e
kids-app server <server@uniplay-kids.ru>
2026-07-05 14:05:25 +0000

    Add Australia/New Zealand interactive as 4th Spotlight 5 activity

--- components/LessonBuilder/ActivityRenderers/SpotlightHtmlActivityRenderer.tsx
+++ components/LessonBuilder/ActivityRenderers/SpotlightHtmlActivityRenderer.tsx
@@ -3,6 +3,7 @@ import SpotlightVerbBeSlides from './SpotlightVerbBeSlides';
 import SpotlightS5Alphabet from './SpotlightS5Alphabet';
 import SpotlightS5WeLearnEnglish from './SpotlightS5WeLearnEnglish';
 import SpotlightS5UkUsa from './SpotlightS5UkUsa';
+import SpotlightS5AustraliaNz from './SpotlightS5AustraliaNz';
 import SpotlightEx1 from './SpotlightEx1';
 import SpotlightEx2 from './SpotlightEx2';
 import SpotlightEx3 from './SpotlightEx3';
@@ -70,6 +71,7 @@ const SPOTLIGHT_COMPONENTS: Record<string, React.ComponentType<SlideProps>> = {
   'spotlight5-alphabet': SpotlightS5Alphabet,
   'spotlight5-we-learn-english': SpotlightS5WeLearnEnglish,
   'spotlight5-uk-usa': SpotlightS5UkUsa,
+  'spotlight5-australia-nz': SpotlightS5AustraliaNz,
   'verb-be-ex1': SpotlightEx1,
   'verb-be-ex2': SpotlightEx2,
   'verb-be-ex3': SpotlightEx3,
